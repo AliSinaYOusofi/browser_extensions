e8578d5f8a9650c070b45c4cb03fe0cf0f84b35d
ShahAli <senayousofiali@gmail.com>
2023-05-08 12:00:15 +0500

    does not update time bug"

--- Time_Track/service/background.js
+++ Time_Track/service/background.js
@@ -11,6 +11,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
             keys.forEach( id => {
                 if (data[id].currentLocation === previousDomain) {
                     
+                    console.log("updating previous data");
                     let previousHasHours = data[id].timeSpent?.split(":").length === 3;
                     let newHasHours = request.data.timeSpent?.split(":").length === 3;
     
@@ -23,27 +24,39 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
                     if (previousHasHours && newHasHours) {
                         // for the hour part
                         [previousHr, previousMin, previousSec] = previousTimeSpent;
-                        console.log(previousHr, previousMin, previousSec, 'previous has hours')
                         [ newHr, newMin, newSec] = newTimeSpent;
                     }
                     else {
                         // minute and second part
-                        [previousMin, previousSec] = previousTimeSpent;
                         [newMin, newSec] = newTimeSpent
-                        
-                        let totalSeconds = previousSec + newSec;
-                        let remainingSeconds = totalSeconds % 60;
-                        let totalMinutes = previousMin + newMin + Math.floor(totalSeconds / 60);
-                        let remainingMinutes = totalMinutes % 60;
 
-                        console.log(`Total time spent: ${remainingMinutes} minutes and ${remainingSeconds} seconds`);
-
-                        console.log("**************************END*********************");
+                        if (data[id].timeSpent) {
+                            [previousMin, previousSec] = previousTimeSpent;
+                        }
                         
+                        let updatedTimeSpent
+                        totalSeconds = parseInt(previousSec) + 5
+                        
+                        
+                        if (totalSeconds >= 60) {
+                            previousMin = parseInt(previousMin) + 1
+                            totalSeconds = 0;
+
+                        }
+                        console.log(previousMin + ":" + previousSec, " prev");
+                        updatedTimeSpent = `${previousMin.toString().padStart(2, '0')}:${((parseInt(previousSec) + parseInt(newSec)) % 60).toString().padStart(2, '0')}`;
+
+                        console.log(updatedTimeSpent, ' new');
+                        data[id].timeSpent = updatedTimeSpent;
+
+                        chrome.storage.local.set({ [id]: data[id] }, () => {
+                            sendResponse({ success: true });
+                        });
+
+                        return;
                     }
                 }
             })
-
             chrome.storage.local.set({ [request.data.currentLocation]: request.data }, function() {
                 sendResponse({ success: true });
             });
